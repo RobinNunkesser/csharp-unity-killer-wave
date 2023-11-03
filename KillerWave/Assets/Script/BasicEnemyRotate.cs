52af96934fe693074f2390ac7abebf382fc65903
nunkesser <robin.nunkesser@hshl.de>
2023-11-03 10:28:24 +0100

    Chapter 4

---
+++ KillerWave/Assets/Script/BasicEnemyRotate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyRotate : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left*Time.deltaTime*speed);
+    }
+}
